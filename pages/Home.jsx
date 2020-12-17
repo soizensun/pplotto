050f8ae9d267d330b05c19a835acea9799e7bb5e
ap13738708 <parappp@hotmail.com>
2020-12-17 13:10:49 +0700

    Edit close time

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -30,7 +30,7 @@ export default function Home() {
   const [deletedData, setDeletedData] = useState([])
   const [currentData, setCurrentData] = useState("")
   const [InputFieldStatus, setInputFieldStatus] = useState(true)
-  const [closeTime, setCloseTime] = useState("14.30 น.")
+  const [closeTime, setCloseTime] = useState("14.00 น.")
 
   useEffect(() => {
     if (localStorage.getItem('currentUser') == null) location.href = '/'
